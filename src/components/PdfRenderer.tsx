302aaddadcebeea5c32e430b7121c17c49252aff
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-07-02 20:36:43 +0300

    Changed pdf worker for production build

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -19,10 +19,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import PdfFullScreen from "./PdfFullScreen";
 
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface PdfRendererProps {
   url: string
