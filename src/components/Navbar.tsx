81cd3dd292b02a140f11b90232b45d1b0baf899a
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-07-02 21:23:13 +0300

    Updated navbar accounts for user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,13 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
 import { SignInButton, SignUpButton } from "@clerk/nextjs"
 import { ArrowRight } from "lucide-react"
+import { currentUser, } from "@clerk/nextjs/server"
+import UserAccountNav from "./UserAccountNav"
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const user = await currentUser()
+
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
@@ -16,34 +21,56 @@ const Navbar = () => {
                     <span>intelliPDF</span>
                 </Link>
                 <div className="hidden items-center space-x-4 sm:flex">
-                    <>
-                        <Link 
-                            href="/pricing"
+                    {!user ? (
+                        <>
+                            <Link 
+                                href="/pricing"
+                                className={buttonVariants({
+                                    variant: "ghost",
+                                    size: "sm",
+                                })}
+                            >
+                                Pricing
+                            </Link>
+                            <SignInButton
+                            //@ts-ignore
                             className={buttonVariants({
                                 variant: "ghost",
                                 size: "sm",
                             })}
-                        >
-                            Pricing
-                        </Link>
-                        <SignInButton
-                           //@ts-ignore
-                           className={buttonVariants({
-                            variant: "ghost",
-                            size: "sm",
-                        })}
-                        >
-                            Sign in
-                        </SignInButton>
-                        <SignUpButton
-                           //@ts-ignore
-                           className={buttonVariants({
-                            size: "sm",
-                        })}
-                        >
-                            Get started 
-                        </SignUpButton>
-                    </>
+                            >
+                                Sign in
+                            </SignInButton>
+                            <SignUpButton
+                            //@ts-ignore
+                            className={buttonVariants({
+                                size: "sm",
+                            })}
+                            >
+                                Get started 
+                            </SignUpButton>
+                        </>
+                    ) : (
+                        <>
+                            <Link 
+                                    href="/dashboard"
+                                    className={buttonVariants({
+                                        variant: "ghost",
+                                        size: "sm",
+                                    })}
+                                >
+                                    Dashboard
+                            </Link>
+
+                            <UserAccountNav 
+                                name={
+                                    !user.firstName || !user.fullName ? "Your Account" : `${user.fullName}`
+                                }
+                                email={user.primaryEmailAddress?.emailAddress}
+                                imageUrl={user.imageUrl}
+                            />
+                        </>
+                    )}
                 </div>
             </div>
         </MaxWidthWrapper>
