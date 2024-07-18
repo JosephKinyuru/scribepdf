287f8d5afe00fff42e910d442c6be3c97a33cd3d
joseph-kinyuru <josephkinyuru22@gmail.com>
2024-07-18 15:43:18 +0300

    Removed console for url for PDF

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -65,8 +65,6 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
     setValue("page", String(page))
   }
 
-  console.log(url)
-
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
